806ca8585150067c47244bc0a6b4550876974638
hyewon do <tsc03018@gmail.com>
2022-06-29 21:25:31 +0900

    feat: 무한스크롤 구현

--- src/VirtualizedList.jsx
+++ src/VirtualizedList.jsx
@@ -9,15 +9,35 @@ import {
 import loadMoreData from "./makeData";
 import "./VirtualizedList.css";
 
+const size = 30;
+
 export default function VirtualizedList() {
-  const dataRef = useRef(loadMoreData(0));
+  const curIndex = useRef(1);
+  const isFetching = useRef(false);
+  const [data, setData] = useState(loadMoreData(0));
   const [cache] = useState(
     new CellMeasurerCache({ fixedWidth: true, defaultHeight: 600 })
   );
 
+  const loadMoreRows = ({ startIndex, stopIndex }) => {
+    if (!isFetching.current && stopIndex > curIndex.current * size) {
+      console.log("LOADING", startIndex, stopIndex, curIndex.current);
+      isFetching.current = true;
+
+      setData([...data, ...loadMoreData(curIndex.current)]);
+      curIndex.current = curIndex.current + 1;
+      isFetching.current = false;
+    } else {
+      return new Promise((resolve) => {
+        resolve();
+      });
+    }
+  };
+
   const RenderRow = useCallback(
     ({ key, index, style, parent }) => {
-      const data = dataRef.current[index];
+      const rowData = data[index];
+
       return (
         <CellMeasurer
           key={key}
@@ -26,32 +46,54 @@ export default function VirtualizedList() {
           rowIndex={index}
           cache={cache}
         >
-          <div className="cell" style={style}>
-            {data.name}
-            <div style={{ height: `${data.height * 10}px` }} />
-          </div>
+          {rowData ? (
+            <div className="cell" style={style}>
+              {rowData.name}
+              <div style={{ height: `${rowData.height * 10}px` }} />
+            </div>
+          ) : (
+            <div className="cell" style={style}>
+              Loading...
+            </div>
+          )}
         </CellMeasurer>
       );
     },
-    [dataRef.current]
+    [data, cache]
   );
+
+  if (!data.length) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="container">
       <AutoSizer>
         {({ width, height }) => {
           return (
-            <List
-              width={width}
-              height={height}
-              rowCount={40}
-              rowHeight={({ index }) => {
-                return cache.rowHeight({ index });
+            <InfiniteLoader
+              isRowLoaded={(param) => {
+                return false;
               }}
-              deferredMeasurementCache={cache}
-              direction="vertical"
-              rowRenderer={RenderRow}
-            />
+              rowCount={100}
+              loadMoreRows={loadMoreRows}
+            >
+              {({ onRowsRendered, registerChild }) => (
+                <List
+                  width={width}
+                  height={height}
+                  rowCount={100}
+                  rowHeight={({ index }) => {
+                    return cache.rowHeight({ index });
+                  }}
+                  ref={registerChild}
+                  onRowsRendered={onRowsRendered}
+                  deferredMeasurementCache={cache}
+                  direction="vertical"
+                  rowRenderer={RenderRow}
+                />
+              )}
+            </InfiniteLoader>
           );
         }}
       </AutoSizer>
--- src/makeData.js
+++ src/makeData.js
@@ -5,14 +5,13 @@ const chanceInstance = new Chance();
 
 // 요청이 들어올때마다 랜덤 데이터를 20개씩 생성해줌
 const generateRandomData = (pageNumber) => {
-  return Array.from({ length: 40 }).map((_, index) => ({
+  return Array.from({ length: 30 }).map((_, index) => ({
     id: uuid(),
-    name: `${pageNumber + 1}번쨰로 호출한 데이터 :  ${chanceInstance.name()}`,
+    name: `${pageNumber}번쨰로 호출한 데이터 :  ${chanceInstance.name()}`,
     height: Math.ceil(Math.random() * 10),
   }));
 };
 
-// 비동기 처리 필요
 export default function loadMoreData(index) {
   return generateRandomData(index);
 }
